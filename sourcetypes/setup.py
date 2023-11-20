c8811164c0df8446cbd58d08dc63a03a32736881
Patrick Arminio <patrick.arminio@gmail.com>
2023-11-20 10:26:04 +0000

    Add py.typed file

--- sourcetypes/setup.py
+++ sourcetypes/setup.py
@@ -14,6 +14,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/samwillis/python-inline-source/sourcetypes",
     py_modules=['sourcetypes'],
+    package_data={'sourcetypes': ['py.typed']},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
